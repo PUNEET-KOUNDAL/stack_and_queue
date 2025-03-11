f0be18ed689a8cb7e02a7065ffbbbce74245315a
Puneet Koundal <puneetkoundal707@gmail.com>
2025-03-11 21:55:40 +0530

    add test cases for stacs

---
+++ teststack.py
@@ -0,0 +1,14 @@
+from Stack_LIST_with_nolimit import Stack 
+
+
+stack = Stack()
+stack.push(1)
+stack.push(2)   
+
+print(stack)
+
+print( stack.isEmpty())
+
+print(stack.peek())
+
+print(stack.delete())
